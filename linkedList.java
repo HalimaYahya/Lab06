7e2ef08334ed7d02c71f399b5588908fc611ba84
Halima <201720504@ammanu.edu.jo>
2021-11-29 16:04:14 +0400

    I add   new file:   linkedList.java

---
+++ linkedList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class testArray
+{
+   public static void main(String [] args)
+   {
+        private ArrayList myList;
+
+    public double Average (ArrayList values)
+    { 
+  
+      ArrayList myList = new ArrayList<Double>(2.3, 5.0, 11.0 ,17.0, 100.0,0.7 );
+          for (double i : myList) {
+           System.out.println("The values: "+i);
+           }
+     }
+
+         
+         System.out.println(i);
+   }
+}
