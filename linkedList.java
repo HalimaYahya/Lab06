1dfb6d54372f74fe724aca68319e6b6bef69f2b7
Halima <201720504@ammanu.edu.jo>
2021-11-29 16:47:34 +0400

    I modified:   linkedList.java

--- linkedList.java
+++ linkedList.java
@@ -3,18 +3,75 @@ public class testArray
 {
    public static void main(String [] args)
    {
-        private ArrayList myList;
-
-    public double Average (ArrayList values)
-    { 
+blic class MinMax {  
+    class Node{  
+        int number;  
+        Node nextNu;  
   
-      ArrayList myList = new ArrayList<Double>(2.3, 5.0, 11.0 ,17.0, 100.0,0.7 );
-          for (double i : myList) {
-           System.out.println("The values: "+i);
-           }
-     }
+        public Node(int number) {  
+            this.number = number;  
+            this.nextNu = null;  
+        }  
+    }  
+    public Node head = null;  
+    public Node tail = null;  
+    public void addNode(int number) {
+        Node newNode = new Node(number);  
+        if(head == null) {  
+            head = newNode;  
+            tail = newNode;  
+        }  
+        else {   
+            tail.nextNu = newNode;  
+            tail = newNode;  
+        }  
+    }  
+    public void minNode() {  
+        Node current = head;  
+        int min;  
+        if(head == null) {  
+            System.out.println("List is empty");  
+        }  
+        else {  
+            min = head.number;  
+            while(current != null){   
+                 if(min > current.number) {  
+                     min = current.number;  
+                 }  
+                 current= current.nextNu;  
+            }  
+            System.out.println("Minimum value : "+ min);  
+        }  
+    }  
+    public void maxNode() {  
+        Node current = head;  
+        int max;  
+  
+        if(head == null) {  
+            System.out.println("List is empty");  
+        }  
+        else {  
+            max = head.number;  
+  
+            while(current != null){  
+                 if(max < current.number) {  
+                     max = current.number;  
+                 }  
+                 current = current.nextNu;  
+            }  
+            System.out.println("Here is a huge integer value node in the list: "+ max);  
+        }  
+    }  
+    public static void main(String[] args) {  
+        MinMax lList = new MinMax();  
+        lList.addNode((2*(10*10*10)));  
+        lList.addNode(0*(10*10));  
+        lList.addNode(1*(10));  
+        lList.addNode(1*(10));  
 
-         
-         System.out.println(i);
+        lList.minNode();  
+        lList.maxNode();  
+    }  
+}  
    }
 }
